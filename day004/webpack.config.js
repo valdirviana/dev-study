108338195a98707e1c091cb0ef65be1e6dc829b4
Valdir Lopes Viana <valdir.viana@hbsis.com.br>
2017-10-09 13:03:42 -0300

    First steps of the skelleton

---
+++ day004/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/app.html',
+  filename: 'app.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+
+};
+
+
