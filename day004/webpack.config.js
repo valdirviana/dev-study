29fcb8ebd8f8bb45bc5e92a31ed6faf50bfaeb5f
Valdir Lopes Viana <valdir.viana@hbsis.com.br>
2017-11-01 17:03:46 -0200

    novos estudos

--- day004/webpack.config.js
+++ day004/webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/app.html',
-  filename: 'app.html',
+  template: './src/index.html',
+  filename: 'index.html',
   inject: 'body'
 })
 
