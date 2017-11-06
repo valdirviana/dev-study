d658e09e0284d14cac3293ce02256c02168b1f1b
Valdir Lopes Viana <valdir.viana@hbsis.com.br>
2017-11-06 15:59:51 -0200

    tests in dotnet core

---
+++ day008/app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = new List<string>(){};
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
