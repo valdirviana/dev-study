0a2fd4a58b0233463fa6ed20a9a12dfb719af636
Valdir Lopes Viana <valdir.viana@hbsis.com.br>
2017-11-06 16:52:08 -0200

    dotnet

--- day008/app/Program.cs
+++ day008/app/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace app
@@ -9,6 +10,17 @@ namespace app
         {
             var teste = new List<string>(){};
             Console.WriteLine("Hello World!");
+
+            JArray array = new JArray();
+            array.Add("Manual text");
+            array.Add(new DateTime(2000, 5, 23));
+
+            JObject o = new JObject();
+            o["MyArray"] = array;
+            
+            string json = o.ToString();
+
+            var teste1 = new Tuple<string,int>("",2);
         }
     }
 }
