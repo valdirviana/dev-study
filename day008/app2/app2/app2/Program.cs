38974e267469cf01dcec14a966174ad8a1ba985f
Valdir Lopes Viana <valdir.viana@hbsis.com.br>
2017-11-08 10:45:06 -0200

    day 009

---
+++ day008/app2/app2/app2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace app2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            JArray array = new JArray();
+            array.Add("Manual text");
+            array.Add(new DateTime(2000, 5, 23));
+
+            JObject o = new JObject
+            {
+                ["MyArray"] = array
+            };
+
+            string json = o.ToString();
+        }
+    }
+}
---
+++ day008/app2/app2/app3/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace app3
+{
+    public class Class1
+    {
+    }
+}
